87979c81a74626f361b602f9a7cf8a677ae89a12
TAREK BAKR <tarekfathi2003@gmail.com>
2021-06-20 13:21:59 +0300

    SysOwner initialization

---
+++ SysOwner-WS/sysowner/src/test/java/com/pickup/sysowner/SysownerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pickup.sysowner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysownerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
